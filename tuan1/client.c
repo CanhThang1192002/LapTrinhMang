9e37b2e4b321942d5f3e0120c5550bc37965c2c9
Canh Thang <104011289+CanhThang1192002@users.noreply.github.com>
2023-10-27 11:26:34 +0700

    Update client.c

--- tuan1/client.c
+++ tuan1/client.c
@@ -1,1 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <openssl/sha.h>
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s IPAddress PortNumber\n", argv[0]);
+        exit(1);
+    }
+
+    int port = atoi(argv[2);
+
+    int client_socket = socket(AF_INET, SOCK_DGRAM, 0);
+    if (client_socket == -1) {
+        perror("Socket creation error");
+        exit(1);
+    }
+
+    struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+    server_addr.sin_addr.s_addr = inet_addr(argv[1]);
+
+    while (1) {
+        // Allow the user to input account, password, and new password
+        // Send the input data to the server
+        // Receive and display the server's response
+        // Implement other functionality like signout
+
+        // Exit the client on some condition
+    }
+
+    close(client_socket);
+    return 0;
+}
 
